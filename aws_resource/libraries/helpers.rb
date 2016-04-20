21c39a214a36f3158f7c0242d41c840e3f1bf947
Munu33 <munusamy04@gmail.com>
2016-04-20 21:47:15 +0530

    resolved issue for gathering security groups

--- aws_resource/libraries/helpers.rb
+++ aws_resource/libraries/helpers.rb
@@ -25,10 +25,14 @@ module Vpc
         res.instances.each do |ints|
           if ints.state.name == "running"
             runners = {}
+            sg_arry = []
             runners['inst'] = ints.instance_id
             runners[:vpc_id] = ints.vpc_id
             runners[:subnet_id] = ints.subnet_id
-            ints.security_groups.each {|sg| runners[:security_groups] = sg.group_id }
+            ints.security_groups.each do |sg| 
+              sg_arry << sg.group_id
+              runners[:security_groups] = sg_arry 
+            end
             @instance_running << runners
           else
             @instance_stopped << ints.instance_id
@@ -36,29 +40,35 @@ module Vpc
         end
       
       end
-      
       vpc_ids = (@instance_running.uniq {|e| e[:vpc_id]}).map {|k| k[:vpc_id]}
       
-      @vpc_item = []
       @vpc_hash = {}
       vpc_ids.each do |file|
         @subnet_list = []
         @instances_list = []
+        @security_list = []
+#binding.pry
         @instance_running.each do |ins|
-          if file == ins[:vpc_id]
-            @vpc_hash['id'] = "vpc_resp_#{file}"
-            @vpc_hash[:vpc_id] = ins[:vpc_id]
+#binding.pry
+        if file == ins[:vpc_id]
+          @vpc_hash['id'] = "vpc_resp_#{file}"
+          @vpc_hash[:vpc_id] = ins[:vpc_id]
        	unless @subnet_list.include?(ins[:subnet_id])
           @subnet_list << ins[:subnet_id]
       	end	
+        unless @security_list.include?(ins[:security_groups])
+          @security_list << ins[:security_groups]
+        end
       	@instances_list << ins['inst']
-          else
-            puts "vpc is not matching"
-          end
-            @vpc_hash[:subnet_ids] = @subnet_list
-            @vpc_hash[:instances] = @instances_list
-            @vpc_hash[:security_groups] = ins[:security_groups]
+        else
+          puts "vpc is not matching"
+        end
+          @vpc_hash[:subnet_ids] = @subnet_list
+          @vpc_hash[:instances] = @instances_list
+          @vpc_hash[:security_groups] = @security_list.flatten
         puts @vpc_hash
+        
+        end
         unless Chef::DataBag.list.key?('vpc_resp')
         vpc_save = Chef::DataBag.new
         vpc_save.name('vpc_resp')
@@ -69,7 +79,6 @@ module Vpc
         databag_item.data_bag('vpc_resp')
         databag_item.raw_data = @vpc_hash
         databag_item.save
-        end
       end
     end
     
